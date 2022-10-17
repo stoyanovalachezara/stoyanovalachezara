83b2e41d78a01b0d82b581bcc165d776769348bf
stoyanovalachezara <90977447+stoyanovalachezara@users.noreply.github.com>
2022-10-18 00:02:00 +0200

    Add files via upload

---
+++ Stoyanova_stringa.py
@@ -0,0 +1,12 @@
+saluto = "Ciao"
+virgola = ","
+spazio = " "
+presentazione = "sono"
+nome = "Lachezara"
+congiunzione = "e"
+domanda = "tu?"
+
+frase = saluto + virgola + spazio + presentazione + spazio + nome + spazio + congiunzione + spazio + domanda
+
+print(frase)
+print()
